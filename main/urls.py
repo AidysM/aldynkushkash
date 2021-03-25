f6fb7028a6ac6b3237c97279be13d7b61046578e
AidysM <mongush10011979@gmail.com>
2021-03-26 02:35:58 +0700

    created logout, profile

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import other_page
-from .views import index, AKLoginView
+from .views import index, AKLoginView, profile, AKLogoutView
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/logout/', AKLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', AKLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
