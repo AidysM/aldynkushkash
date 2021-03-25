3e968800cff111bf191e9f799b9f4c568ddb78e5
AidysM <mongush10011979@gmail.com>
2021-03-25 17:45:29 +0700

    created aldynkush.data

--- aldynkushkash/settings.py
+++ aldynkushkash/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'aldynkushkash.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'aldynkushkash.data',
+        'NAME': BASE_DIR / 'aldynkush.data',
     }
 }
 
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'main.AdvUser'
