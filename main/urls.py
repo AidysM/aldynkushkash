8e2f650d1a2bf047da68df4131248971dcb702a2
AidysM <mongush10011979@gmail.com>
2021-03-26 03:22:53 +0700

    created password_change

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import other_page
-from .views import index, AKLoginView, profile, AKLogoutView, ChangeUserInfoView
+from .views import index, AKLoginView, profile, AKLogoutView, ChangeUserInfoView, AKPasswordChangeView
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/logout/', AKLogoutView.as_view(), name='logout'),
+    path('accounts/password/change/', AKPasswordChangeView.as_view(), name='password_change'),
     path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', AKLoginView.as_view(), name='login'),
