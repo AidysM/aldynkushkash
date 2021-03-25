992d969dedbab06a006f7867a539d395e11531c4
AidysM <mongush10011979@gmail.com>
2021-03-26 01:26:00 +0700

    created login

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import other_page
-from .views import index
+from .views import index, AKLoginView
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/login/', AKLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
 ]
