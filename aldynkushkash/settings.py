19d95b68cd08095cfa5f45983eee43024fae0389
AidysM <mongush10011979@gmail.com>
2021-03-26 06:07:48 +0700

    created activation

--- aldynkushkash/settings.py
+++ aldynkushkash/settings.py
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'main.AdvUser'
+
+EMAIL_PORT = 1025
--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from .views import other_page
+from .views import other_page, RegisterUserView, RegisterDoneView, user_activate
 from .views import index, AKLoginView, profile, AKLogoutView, ChangeUserInfoView, AKPasswordChangeView
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/register/activate/<str:sign>/', user_activate, name='register_activate'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
     path('accounts/logout/', AKLogoutView.as_view(), name='logout'),
     path('accounts/password/change/', AKPasswordChangeView.as_view(), name='password_change'),
     path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
+
     path('accounts/login/', AKLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
