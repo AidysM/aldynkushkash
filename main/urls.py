852175d6345e165e9f8dbd136c05216c4490042f
AidysM <mongush10011979@gmail.com>
2021-03-26 03:10:13 +0700

    created profile_change

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import other_page
-from .views import index, AKLoginView, profile, AKLogoutView
+from .views import index, AKLoginView, profile, AKLogoutView, ChangeUserInfoView
 
 app_name = 'main'
 urlpatterns = [
     path('accounts/logout/', AKLogoutView.as_view(), name='logout'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', AKLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
