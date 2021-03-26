26484fc763a425f6a5022c7854f900339e721d77
AidysM <mongush10011979@gmail.com>
2021-03-27 01:19:23 +0700

    created advuseradmin

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import other_page, RegisterUserView, RegisterDoneView, user_activate
+from .views import other_page, RegisterUserView, RegisterDoneView, user_activate, DeleteUserView
 from .views import index, AKLoginView, profile, AKLogoutView, ChangeUserInfoView, AKPasswordChangeView
 
 app_name = 'main'
@@ -9,9 +9,9 @@ urlpatterns = [
     path('accounts/register/', RegisterUserView.as_view(), name='register'),
     path('accounts/logout/', AKLogoutView.as_view(), name='logout'),
     path('accounts/password/change/', AKPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
     path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
-
     path('accounts/login/', AKLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
